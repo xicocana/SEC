844e4decd4a63b7b6cf100140be46f44c0271269
xicocana <kanana.traceur@gmail.com>
2019-03-11 16:13:18 +0000

    interface client inicio

--- sec/notary/client/ws/NotaryClient.java
+++ sec/notary/client/ws/NotaryClient.java
@@ -22,17 +22,52 @@ public class NotaryClient {
 
         System.out.println("NotaryClient running with id: " + input);
 
+        System.out.println("------------------------------ ");
+        System.out.println("NotaryClient option:");
+        System.out.println("1 -> intentToSell(owner, good)");
+        System.out.println("2 -> getStateGood(good)");
+        System.out.println("3 -> buyGood(???)");
+        System.out.println("4 -> exit");
 
-        List<String> users = notaryWebservice.getUsers();
+        System.out.print("select option: ");
+        String opt = System.console().readLine();
 
-        for (String s : users) {
-            System.out.println(s);
+        boolean flag = true;
+        while(flag){
+            String goodid;
+            switch(Integer.parseInt(opt)) {
+                case 1 :
+                    System.out.print("insert good ID: ");
+                    goodid = System.console().readLine();
+                    boolean b = notaryWebservice.intentionToSell(input,goodid);
+                    System.out.println(b);
+                    System.out.println(" ");
+                    break;
+                case 2 :
+                    System.out.print("insert good ID: ");
+                    goodid = System.console().readLine();
+                    String res = notaryWebservice.getStateOfGood(goodid);
+                    System.out.println(res);
+                    System.out.println(" ");
+                    break;
+                case 3 :
+                    System.out.println("selected option 3");
+                    System.out.println("work in progress...");
+                    System.out.println(" ");
+                    break;
+                case 4:
+                    flag = false;
+                    System.out.println("notary client terminated.");
+                    System.out.println(" ");
+                    break;
+                default :
+                    System.out.println("Invalid option");
+                    System.out.println(" ");
+            }
+            if(flag){
+                System.out.print("select option: ");
+                opt = System.console().readLine();
+            }
         }
-
-        String res = notaryWebservice.getStateOfGood("good0");
-        System.out.println(res);
-
-        boolean b = notaryWebservice.intentionToSell(input,"good0");
-        System.out.println(b);
     }
 }
